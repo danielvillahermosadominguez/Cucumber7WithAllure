a3b58d9d39361eb42cbf18a8833f74865fdd8f62
Daniel <daniel.villahermosa@codurance.com>
2022-08-24 17:58:57 +0200

    script for compile and send to allure two repos

--- updateAllure.sh
+++ updateAllure.sh
@@ -6,11 +6,16 @@ token=$(echo $json | grep -o '"access_token":"[^"]*' | grep -o '[^"]*$')
 curl -X GET http://localhost:7272/allure-docker-service/clean-results -b cookiesFile -ik
 rm ../../../target/allure-results/*
 
-echo compiling first maven project
+echo compiling maven projects
 mvn clean test
+mvn -f second-repo/pom.xml clean test
+
 
 echo copying results of first maven project
 cp ./target/allure-results/* ../../../target/allure-results
+cp ./second-repo/target/allure-results/* ../../../target/allure-results
+
+
 
 echo generating report
 curl -X GET http://localhost:7272/allure-docker-service/generate-report -b cookiesFile -ik
